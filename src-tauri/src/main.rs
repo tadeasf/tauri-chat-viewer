71bc9dc80450199eeabc285d161f1bf60092ab05
tadeasf <taddy.fort@gmail.com>
2023-08-11 17:50:52 +0200

    tauri opens server broken

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::process::{Command, Stdio};
-use std::env;
+use log::{info, error};
 
 fn main() {
-    // Set the path to the express-server directory
-    let current_dir = env::current_dir().unwrap();
-    let express_server_path = current_dir.parent().unwrap().join("express-server");
+    // Initialize the logger
+    simple_logger::init().unwrap();
 
-    // Print out the resolved path for debugging
-    println!("Resolved Express server path: {:?}", express_server_path);
+    info!("Starting the Tauri app...");
 
-    // Start the Express server
-    let _child = Command::new("node")
-        .arg("index.js")
-        .current_dir(&express_server_path) // Set the current directory
-        .stdout(Stdio::inherit()) // Optional: To see the Express server logs in the Tauri console
-        .spawn()
-        .expect("Failed to start Express server");
-
-    // Run the Tauri app
     tauri::Builder::default()
+        .setup(|app| {
+            // Use Tauri's API to resolve the path to the bundled express-server directory
+            let express_server_path = app.path_resolver()
+                .resolve_resource("./../express-server")
+                .expect("Failed to resolve express-server path in resources");
+            
+            info!("Resolved Express server path: {:?}", express_server_path);
+
+            // Start the Express server
+            let child = Command::new("node")
+                .arg("index.js")
+                .current_dir(&express_server_path) // Set the current directory
+                .stdout(Stdio::inherit()) // Optional: To see the Express server logs in the Tauri console
+                .spawn();
+
+            match child {
+                Ok(_) => info!("Express server started successfully."),
+                Err(e) => error!("Failed to start Express server: {:?}", e),
+            }
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
