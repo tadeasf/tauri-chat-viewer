ae2a77d3b0a607a089e00ec86d76015439563a41
tadeasf <taddy.fort@gmail.com>
2023-08-11 16:21:58 +0200

    relative pathing tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,16 @@ use std::env;
 
 fn main() {
     // Set the path to the express-server directory
-    let express_server_path = env::current_dir().unwrap().join("/Users/tadeasfort/Documents/pythonJSprojects/gitHub/tauri-chat-viewer/express-server");
+    let current_dir = env::current_dir().unwrap();
+    let express_server_path = current_dir.parent().unwrap().join("express-server");
+
+    // Print out the resolved path for debugging
+    println!("Resolved Express server path: {:?}", express_server_path);
 
     // Start the Express server
     let _child = Command::new("node")
         .arg("index.js")
-        .current_dir(express_server_path) // Set the current directory
+        .current_dir(&express_server_path) // Set the current directory
         .stdout(Stdio::inherit()) // Optional: To see the Express server logs in the Tauri console
         .spawn()
         .expect("Failed to start Express server");
