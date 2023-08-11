9e364604ca2dcc324e96348386204083c85e2164
tadeasf <taddy.fort@gmail.com>
2023-08-11 21:49:37 +0200

    asd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use log::info;
+use std::env;
 
 fn main() {
-    // Initialize the logger
-    simple_logger::init().unwrap();
-
-    info!("Starting the Tauri app...");
-
+    env::set_var("RUST_BACKTRACE", "full");
+    // Run the Tauri app
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
