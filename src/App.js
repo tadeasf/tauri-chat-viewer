b7d598b909f3441a5479a2b35447ba5e18b55991
tadeasf <taddy.fort@gmail.com>
2024-03-23 22:27:20 +0100

    Refactor gallery layout in App.js

--- src/App.js
+++ src/App.js
@@ -877,7 +877,12 @@ const App = observer(() => {
                 className="overflow-auto p-4 max-h-full w-full"
                 style={{ maxHeight: "90%" }}
               >
-                <div className="grid grid-cols-3 gap-0">
+                <div
+                  className="grid gap-0"
+                  style={{
+                    gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+                  }}
+                >
                   {galleryPhotos.map((photo, index) => (
                     <div
                       key={photo.creation_timestamp}
@@ -904,26 +909,7 @@ const App = observer(() => {
           )}
           {galleryLoading && (
             <div className="fixed inset-0 bg-black bg-opacity-75 z-50 flex justify-center items-center">
-              <svg
-                className="animate-spin h-8 w-8 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
+              {/* Loading Spinner */}
             </div>
           )}
         </div>
